f3ea0250776bbc4801dcd42dbd052df6e286bf06
Brianne Bourassa <shinybri1011@gmail.com>
2022-08-26 13:34:24 -0600

    Add self-evaluation.md and added array methods to array.js

--- arrays.js
+++ arrays.js
@@ -1,3 +1,11 @@
 var dogs = ["Stella", "Gretta", "Bubba", "Rogue"];
+dogs.pop();
+console.log(dogs);
+
 var training_times = [6, 8, 10, 13, 15];
+training_times.push(16)
+console.log(training_times)
+
 var pass = ["true", "true", "false" "false"]
+pass.shift();
+console.log(pass);
